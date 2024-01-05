e54a18d19454aa0de8a250b76aa020a18cffe57b
AxelitoMixxx <afriash@gbm.com.mx>
2024-01-05 00:53:43 -0600

    Step 1

--- examples/helloWorld/hello-world.go
+++ examples/helloWorld/hello-world.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	a := []int{1, 2, 3}
+	var b []int
+	fmt.Println(a)
+	fmt.Println(b)
+	i := 0
+	a = append(a[:i], a[i+1:]...)
+	b = append(b, a[:1]...)
+	fmt.Println(a)
+	fmt.Println(b)
 	fmt.Println("hello world")
 }
